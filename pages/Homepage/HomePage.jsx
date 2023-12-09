b74e25b1ff8ca30acc1c7dceb8697a8c38f02c4b
Parth Deshpande <parthjun27@gmail.com>
2023-12-09 20:08:43 +0530

    Header& Footer Created

--- pages/Homepage/HomePage.jsx
+++ pages/Homepage/HomePage.jsx
@@ -1,10 +1,19 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
+import FeedPosts from "../../src/components/FeedPosts/FeedPosts";
+
 
 const HomePage = () => {
-  return (
-    <div>
-      HomePage
-    </div>
-  )
+  return<Container maxW={"container.xl"}>
+    <Flex gap={20}>
+      <Box flex={2} py={10} >
+        <FeedPosts/>
+      </Box>
+      <Box flex={3} mr={20}
+      display={{base:"none",lg:"block"}} maxW={"300px"} >
+        SuggestedUsers
+      </Box>
+    </Flex>
+  </Container>;
 }
 
 export default HomePage;
